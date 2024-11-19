d95a7b0b7e1f45550a655ed55505aea163893e83
Stepan Kuhta <farena1804@gmail.com>
2024-11-19 16:40:13 +0300

    Completed

---
+++ TodoServerApp/Data/Migrations/20241119130916_AddRest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 19, 16, 9, 15, 758, DateTimeKind.Local).AddTicks(9493));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 19, 16, 9, 15, 758, DateTimeKind.Local).AddTicks(9510));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 19, 16, 9, 15, 758, DateTimeKind.Local).AddTicks(9512));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 11, 19, 16, 9, 15, 758, DateTimeKind.Local).AddTicks(9514));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "TaskItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 16, 14, 6, 10, 405, DateTimeKind.Local).AddTicks(7794));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 16, 14, 6, 10, 405, DateTimeKind.Local).AddTicks(7811));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 16, 14, 6, 10, 405, DateTimeKind.Local).AddTicks(7814));
+
+            migrationBuilder.UpdateData(
+                table: "TaskItems",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 16, 14, 6, 10, 405, DateTimeKind.Local).AddTicks(7817));
+        }
+    }
+}
